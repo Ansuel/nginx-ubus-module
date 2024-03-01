7f28c5e575c60994dc035e26c4ea6611e95c239b
Christian Marangi <ansuelsmth@gmail.com>
2024-03-02 00:30:42 +0100

    Revert "ngx_http_ubus_module: rework ubus connection to per thread"
    This reverts commit 367fe594261dff19083312858f6423e05d28128e.
    
    Ubus is actually very problematic with concurrent connection. It was
    notice that many problems and deadlock happens in the very rare (but
    actually not that rare due to all the thread implementation and a
    webserver handling multiple connection at the same time) and this also
    seems to cause heap corruption.
    
    Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>

--- src/ubus_utility.h
+++ src/ubus_utility.h
@@ -39,6 +39,7 @@ typedef struct {
 	int res_len;
 	ngx_chain_t *out_chain;
 	ngx_chain_t *out_chain_start;
+	struct ubus_context *ubus_ctx;
 	sem_t *sem;
 	/* Signal when at least a thread is free */
 	sem_t *avail_thread;
@@ -55,7 +56,6 @@ typedef struct {
 } request_ctx_t;
 
 typedef struct {
-	struct ubus_context *ubus_ctx;
 	struct dispatch_ubus *ubus;
 	char **res_str;
 	request_ctx_t *request;
